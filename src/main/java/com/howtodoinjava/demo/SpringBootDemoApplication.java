c0d4e4dc7ad358031db36117426e0f8e57035136
Biswal Dipanka <dipanka.biswal@gmail.com>
2020-03-17 17:34:59 +0530

    modified files

--- src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
+++ src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
@@ -2,6 +2,7 @@ package com.howtodoinjava.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication 
 public class SpringBootDemoApplication extends SpringBootServletInitializer{
